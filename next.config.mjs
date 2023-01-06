952174ddaff4a60d4242639e12555274abff2ee2
Jason Barone <jason@jasonbarone.com>
2023-01-06 14:16:47 -0600

    upgrade dependencies. resolves #1

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,8 @@
 /** @type {import("next").NextConfig} */
+
 const config = {
   experimental: {
     appDir: true,
-  },
-  i18n: {
-    locales: ["en"],
-    defaultLocale: "en",
   },
 };
 export default config;
